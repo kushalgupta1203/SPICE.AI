899802d53e741d29a8efe9952f127e1d53d4adc3
Kushal Gupta <kushalgupta1203@gmail.com>
2025-03-28 14:33:46 +0530

    Update test.py

--- testing/test.py
+++ testing/test.py
@@ -8,12 +8,12 @@ model_path = os.path.join("model", "model.h5")
 model = load_model(model_path)
 
 # Load and preprocess test image
-img_path = "path/to/test/image.jpg"  # Replace with actual image path
+img_path = "C:/Users/kushal/Downloads/solar_panel_testing.jpg" 
 img = image.load_img(img_path, target_size=(224, 224))
 img_array = image.img_to_array(img) / 255.0
 img_array = np.expand_dims(img_array, axis=0)
 
 # Make prediction
 predictions = model.predict(img_array)
-predicted_class = np.argmax(predictions, axis=1)
+predicted_class = np.argmax(predictions, axis=1)[0] 
 print(f"Predicted Class: {predicted_class}")
